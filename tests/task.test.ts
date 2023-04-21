515fb0febc6aae256d40ecd1c1d88251793bdf21
Tim O Keeffe <tokeeffe9@gmail.com>
2023-04-21 11:39:42 -0400

    Misinterpreted the test requirement, updated the delete test and data for it

--- tests/task.test.ts
+++ tests/task.test.ts
@@ -1,7 +1,7 @@
 import { expect, test } from "../base/pomFixture";
 import * as data from "../test-data/user-details.json";
 
-let jsonUserData: { email: string; password: string; expectedNoOfTasks: number; expectedString: string; taskName: string; };
+let jsonUserData: { email: string; password: string; initialNoOfTasks: number; noOfTasksToDelete: number, expectedString: string; taskName: string; };
 
 test.beforeEach(async ({ page, baseURL, landingPage, browserName, loginPage }, testInfo) => {
 
@@ -12,15 +12,20 @@ test.beforeEach(async ({ page, baseURL, landingPage, browserName, loginPage }, t
 })
 
 
-test("TC-04 : Verify user can delete tasks from their account", async ({ myTasksPage }) => {
+test("TC-04 : Verify user can still have a remaining task after deleting multiple tasks", async ({ myTasksPage }) => {
 
-    const expectedNoOfTasks = jsonUserData.expectedNoOfTasks;
-    expect(await myTasksPage.getNumberofTasks()).toBe(expectedNoOfTasks);
-    await myTasksPage.deleteAllTasks();
+    const initialNoOfTasks = jsonUserData.initialNoOfTasks;
+    const remainingTasks = initialNoOfTasks - jsonUserData.noOfTasksToDelete;
+    expect(await myTasksPage.getNumberofTasks()).toBe(initialNoOfTasks);
+
+    for (let i = initialNoOfTasks - 1; i >= remainingTasks; i--) {
+        await myTasksPage.deleteTaskByIndex(i);
+    }
     expect(await myTasksPage.getMyTaskText(0)).toBe(jsonUserData.expectedString);
+    expect(await myTasksPage.getNumberofTasks()).toBe(1);
 
     //Resetting data
-    for (let i = 0; i < expectedNoOfTasks; i++) {
+    for (let i = 2; i <= initialNoOfTasks; i++) {
         await myTasksPage.navigateToTasksAndAddTask();
         await myTasksPage.createYourTask("Task Number " + i);
     }
